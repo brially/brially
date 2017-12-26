c5fe6e44f4b4cdafff38414baae1348f0855d74d
brially <brially@gmail.com>
2017-12-26 17:07:31 -0500

    Fix to Api controller

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => ['auth:api'], 'namespace'=>'Api', 'prefix'=>'api'], function () {
+Route::group(['middleware' => ['auth:api'], 'namespace'=>'A], function () {
 
     Route::resource('/user', 'UserController');
 
